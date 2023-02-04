c5c878b21540822f2954808a0f3bf16b5f628800
sakib Ahamed <saapurbo09@gmail.com>
2023-02-04 17:22:57 +0600

    laminas crud

---
+++ config/modules.config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Mvc\I18n',
+    'Laminas\I18n',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\Db',
+    'Laminas\Form',
+    'Application',
+    'Album'
+];
---
+++ module/Album/src/Form/AlbumForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Album\Form;
+
+use Laminas\Form\Form;
+
+class AlbumForm extends Form
+{
+    public function __construct($name = null)
+    {
+        
+        $this->setAttribute('method', 'POST');
+        parent::__construct('album');
+
+        $this->add([
+            'name' => 'id',
+            'type' => 'hidden',
+        ]);
+        $this->add([
+            'name' => 'title',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Title',
+            ],
+        ]);
+        $this->add([
+            'name' => 'artist',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Artist',
+            ],
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => 'Save',
+                'id'    => 'submitbutton',
+            ],
+        ]);
+    }
+}
